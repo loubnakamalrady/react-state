17951107f5eaf2ddb486c217500def63d9746df7
Loubna Kamal <loubnakamaly1998@gmail.com>
2023-07-28 11:42:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Defining the initial state with (Person, Object, and timeInterval())
+  state = {
+    person: {
+      fullName: 'Harrold Styles',
+      bio: 'Ambitious React Developer',
+      imgSrc: 'https://unsplash.com/photos/UcYBL5V0xWQ',
+      profession: 'React Developer',
+      shows: true
+    },
+    timeInterval: 0
+  }
+
+  // Timer started when component mounted
+  componentDidMount() {
+    //setInterval() is to update timeInterval in every second
+    this.interval = setInterval(() => {
+      // Using prevState to update timeInterval
+      this.setState(prevState => ({
+        timeInterval: prevState.timeInterval + 1
+      }));
+    }, 1000);
+  }
+
+  // Timer is cleared when component unmounts
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  // Toggle shows property of person object
+  toggleShows = () => {
+    this.setState(prevState => ({
+      person: {
+        ...prevState.person,
+        shows: !prevState.person.shows
+      }
+    }));
+  }
+
+  // Render method to display JSX elements
+  render() {
+    // Destructuring
+    const { fullName, bio, imgSrc, profession, shows } = this.state.person;
+    const { timeInterval } = this.state;
+
+    return (
+      <div className="App">
+        <h1>{fullName}</h1>
+        {shows && (
+          <>
+            <img src={imgSrc} alt={fullName} />
+            <p>{bio}</p>
+            <p>{profession}</p>
+          </>
+        )}
+        {/* Button to toggle shows property */}
+        <button onClick={this.toggleShows}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {/* Paragraph element to display time interval */}
+        <p>Time since mount: {timeInterval}s</p>
+      </div>
+    );
+  }
 }
 
 export default App;
